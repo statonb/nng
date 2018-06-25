1113c8c5fd7c0a21e03556e480a950f53c699fde
Unknown <statonb@gmail.com>
2018-06-24 20:58:22 -0400

    Added pipeline demo

---
+++ demo/pipeline/pipeline.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+./pipeline node0 ipc:///tmp/pipeline.ipc & node0=$! && sleep 1
+./pipeline node1 ipc:///tmp/pipeline.ipc "Hello, World!"
+./pipeline node1 ipc:///tmp/pipeline.ipc "Goodbye."
+kill $node0
